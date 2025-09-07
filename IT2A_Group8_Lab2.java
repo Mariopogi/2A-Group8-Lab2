2f91f4b07a27bb389407f7e1ae617de64082ffb2
BicDelacruz <bicdelacruz69420@gmail.com>
2025-09-08 01:35:10 +0800

    Finished search and delete options

--- IT2A_Group8_Lab2.java
+++ IT2A_Group8_Lab2.java
@@ -16,7 +16,6 @@ public class IT2A_Group8_Lab2 {
     static boolean isArrayCreated = false; // variable to track if array exists
     static int currentSize = 0; // tracks how many elements are actually stored
 
-
     public static void main(String[] args) {
         int choice;
 
@@ -54,6 +53,7 @@ public class IT2A_Group8_Lab2 {
                         System.out.println("Error: You must create an array first.\n");
                         break;
                     }
+                    searchElement();
                     break;
                 case 4:
                     displayArray();
@@ -63,6 +63,7 @@ public class IT2A_Group8_Lab2 {
                         System.out.println("Error: You must create an array first.\n");
                         break;
                     }
+                    deleteElement();
                     break;
                 case 0:
                     System.out.println("Exiting the program. Goodbye!");
@@ -99,21 +100,25 @@ public class IT2A_Group8_Lab2 {
         }
 
         while (true) {
-        System.out.print("Enter the size of the array (5 - 20): ");
-        Integer size = getValidatedInput();
+            System.out.print("Enter the size of the array (5 - 20): ");
+            Integer size = getValidatedInput();
 
-        if (size == null) {
-            // invalid input = stay in loop and re-ask
-            continue;
-        } else if (size < 5 || size > 20) {
-            System.out.println("Error: Array size must be between 5 and 20.\n");
-            continue; // keep looping until valid
-        }
+            if (size == null) {
+                // invalid input = stay in loop and re-ask
+                continue;
+            } else if (size < 5 || size > 20) {
+                System.out.println("Error: Array size must be between 5 and 20.\n");
+                continue; // keep looping until valid
+            }
 
-        arr = new int[size];
-        isArrayCreated = true;
-        System.out.println("Array of size " + size + " has been created successfully!\n");
-        break; // exit the loop once array is created
+            arr = new int[size];
+            isArrayCreated = true;
+            System.out.println("Array of size " + size + " has been created successfully!\n");
+
+            System.out.print("\nPress Enter to Continue...");
+            scanner.nextLine();
+
+            break; // exit the loop once array is created
         }
     }
 
@@ -128,7 +133,7 @@ public class IT2A_Group8_Lab2 {
             System.out.println("Error: The array is already full. You cannot insert more elements.\n");
             return;
         }
-        
+
         while (true) {
             System.out.print("Enter integer values to insert (enter -99 to stop): ");
             Integer value = getValidatedInput();
@@ -158,6 +163,9 @@ public class IT2A_Group8_Lab2 {
 
             if (currentSize == arr.length) {
                 System.out.println("The array is now full. No more insertions possible.\n");
+
+                System.out.print("\nPress Enter to Continue...");
+                scanner.nextLine();
                 break;
             }
         }
@@ -166,29 +174,106 @@ public class IT2A_Group8_Lab2 {
     // Helper Method: check if value already exists
     private static boolean contains(int value) {
         for (int i = 0; i < currentSize; i++) {
-            if (arr[i] == value) return true;
+            if (arr[i] == value)
+                return true;
         }
         return false;
     }
 
+    // Helper Method: returns index of requested element in array arr
+    private static int indexOf(int element) {
+        int index = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (element == arr[i])
+                index = i;
+        return index;
+    }
+
     // Display Array Method (Not yet fully fixed - just for testing)
     public static void displayArray() {
-    if (!isArrayCreated) {
-        System.out.println("Error: You must create an array first.\n");
-        return;
+        if (!isArrayCreated) {
+            System.out.println("Error: You must create an array first.\n");
+            return;
+        }
+
+        if (currentSize == 0) {
+            System.out.println("The array is empty. No elements to display.\n");
+            System.out.print("\nPress Enter to Continue...");
+            scanner.nextLine();
+            return;
+        }
+
+        System.out.print("\nArray elements: ");
+        for (int i = 0; i < currentSize; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.print("\n\nPress Enter to Continue...");
+        scanner.nextLine();
+
     }
 
-    if (currentSize == 0) {
-        System.out.println("The array is empty. No elements to display.\n");
-        return;
+    public static void deleteElement() {
+        Integer elementRemoved = null;
+        if (currentSize <= 5) {
+            System.out.println("Error: Cannot delete any more elements. Minimmun array size should be 5");
+        } else {
+            while (elementRemoved == null ) {
+                System.out.print("\nEnter element to delete: ");
+                elementRemoved = getValidatedInput();
+            }
+
+            if (contains(elementRemoved)) {
+
+                int[] newArr = new int[arr.length - 1];
+
+                // ex. arr [1, 2, 3, 4, 5], user wants to delete element "3", its index is
+                // fetched by indexOf(int element)
+                // copy the elements BEFORE the index of the element to be removed [1,2] into
+                // newArr
+                System.arraycopy(arr, 0, newArr, 0, indexOf(elementRemoved));
+
+                // copy the elements AFTER the index of the element to be removed [4,5] into
+                // newArr
+                System.arraycopy(arr, indexOf(elementRemoved) + 1, newArr, indexOf(elementRemoved),
+                        arr.length - indexOf(elementRemoved) - 1);
+
+                // update arr and currentSize
+                arr = newArr;
+                currentSize = newArr.length;
+                System.out.printf("Succesfully deleted element \"%d\" \n", elementRemoved);
+
+                System.out.print("\nPress Enter to Continue...");
+                scanner.nextLine();
+
+            } else
+                System.out.println("Error: Element not found.");
+        }
     }
 
-    System.out.print("Array elements: ");
-    for (int i = 0; i < currentSize; i++) {
-        System.out.print(arr[i] + " ");
-    }
-    System.out.println("\n");
-    }
+    public static void searchElement() {
 
+        if (currentSize == 0) {
+            System.out.println("The array is empty. No elements to search.\n");
+            System.out.print("Press Enter to Continue...");
+            scanner.nextLine();
+            return;
+        }
 
+        Integer choice = null;
+        while (choice == null) {
+            System.out.print("\nEnter Element to Search: ");
+            choice = getValidatedInput();
+        }
+        if (contains(choice)) {
+            System.out.printf("Found element \'%d\' at index %d\n", choice, indexOf(choice));
+            System.out.print("\nPress Enter to Continue...");
+            scanner.nextLine();
+        } else {
+            System.out.printf("Could not find element \'%d\' \n", choice);
+            System.out.print("\nPress Enter to Continue...");
+            scanner.nextLine();
+        }
+
+    }
 }
