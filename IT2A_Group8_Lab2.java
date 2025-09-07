baa5969938d1e0104c4f094b1d91b17ea169785e
Mariopogi <112044412+Mariopogi@users.noreply.github.com>
2025-09-07 09:04:34 +0800

    Initial commit

---
+++ IT2A_Group8_Lab2.java
@@ -0,0 +1,13 @@
+/*  ARRAY OPERATIONS MENU PROGRAM
+    Group 8
+    Authors: Domingo, Mario Jr. C. (Leader)
+             Dela Cruz, Bic Julian (Members)
+             VioVicente, Kevin Christian
+    Laboratory Exercise #2
+    Date: 9/7/2025
+
+*/
+
+public class IT2A_Group8_Lab2 {
+    
+}
